49a9f97b1c349835b61b30e6656bc34fbada6703
José Henrique R. Borges <joseh202017@gmail.com>
2023-04-24 19:05:33 -0300

    App update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css'
 
+import ChatListItem from "./components/ChatListItem";
+
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
 export default () => {
+
+    const [chatlist, setChatList] = useState([{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}])
     return (
         <div className="app-window">
             <div className="sidebar">
                 
+                <header>
+                    <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="avatar image"></img>
+                    <div className="header--buttons">
+                        <div className="header--btn">
+                            <DonutLargeIcon style={{color: '#919191'}} />
+                        </div>
+                        <div className="header--btn">
+                            <ChatIcon style={{color: '#919191'}} />
+                        </div>
+                        <div className="header--btn">
+                            <MoreVertIcon style={{color: '#919191'}} />
+                        </div>
+                    </div>
+                </header>
+
+                <div className="search">
+                    <div className="search--input">
+                        <SearchIcon fontSize="small" style={{color: '#919191'}}/>
+                        <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+                    </div>
+                </div>
+                
+                <div className="chatlist">
+                    {chatlist.map((item, key)=>(
+                        <ChatListItem
+                            key={key}
+                        />
+                    ))}
+                </div>
+
             </div>
             <div className="contentArea">
-
+                CONTENTAREA
             </div>
         </div>
     )
